e6be75ccab53937f62e82aef1e83b1500981b534
NatanFreitas <115927951+MrNatan2023@users.noreply.github.com>
2024-10-30 16:42:51 -0300

    adicionado o codigo base

---
+++ code.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_FRAMES 8 
+#define WORKING_SET_WINDOW 1000
+
+typedef struct {
+    int mBit;   
+    int rBit;    
+    int time;   
+} PageFrame;
+
+int pont = 0;  
+PageFrame memory[NUM_FRAMES];  
+
+
+void verif(int mBit, int time) {
+    int i = 0;
+
+    while (i < NUM_FRAMES) {
+        PageFrame *frame = &memory[pont];  
+
+        if (frame->rBit == 1) {
+            frame->rBit = 0;  
+        } else if (frame->rBit == 0) {
+                frame->mBit = mBit;
+                frame->rBit = 1;
+                frame->time = time;
+                break;
+            }
+
+
+    
+    }
+    
+     pont = (pont + 1) % NUM_FRAMES;
+        i++;
+
+
+       
+}
+
+
+
+void updateMemory(int mBit, int time) {
+    verif(mBit, time);
+}
+
+
+void initMemory() {
+    PageFrame list[NUM_FRAMES] = {
+        {0, 0, 1620},
+        {0, 1, 2032},
+        {0, 1, 2020},
+        {0, 1, 2014},
+        {0, 0, 1213},
+        {0, 1, 1980},
+        {0, 1, 2003},
+        {0, 1, 2084},
+    };
+
+    for (int i = 0; i < NUM_FRAMES; i++) {
+        memory[i] = list[i];
+    }
+}
+
+
+int main() {
+    int mBit = 0;
+    int time = 2204;
+
+    initMemory();
+    updateMemory(mBit, time);
+
+    
+    for (int i = 0; i < NUM_FRAMES; i++) {
+        printf("Memory %d: mBit = %d, rBit = %d, time = %d\n", i, memory[i].mBit, memory[i].rBit, memory[i].time);
+    }
+
+    return 0;
+}
